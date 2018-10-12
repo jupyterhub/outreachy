e84f13073c80498de1e367292dd4314fd5329fc4
GeorgianaElena <georgiana.dolocan@gmail.com>
2018-10-12 12:19:57 +0300

    Test traefik prefix routing using etcd

--- traefik/test_traefikPrefixRouting.py
+++ traefik/test_traefikPrefixRouting.py
@@ -3,57 +3,52 @@ Test
 """
 import traefikUtils
 
-import sys
 import pytest
-import requests
-
-from subprocess import Popen, PIPE
 from os.path import abspath, dirname, join
-
-def traefikRoutesToCorrectBackend(path, expectedPort):
-    baseUrl = "http://localhost:" + str(traefikUtils.getPort("traefik"))
-    resp = requests.get(baseUrl + path)
-    """
-        If we get the expected port, it means traefik
-        routed the request to the right backend
-    """
-    assert int(resp.text) == expectedPort
+from subprocess import Popen, PIPE
 
 def test_routing():
-    traefikPort        = traefikUtils.getPort("traefik")
-    defaultBackendPort = traefikUtils.getPort("defaultBackend")
-    firstBackendPort   = traefikUtils.getPort("firstBackend")
-    secondBackendPort  = traefikUtils.getPort("secondBackend")
-
-    dummyServerPath = abspath(join(dirname(__file__), 'dummyHttpServer.py'))
+    traefikPort     = traefikUtils.getPort("traefik")
     configFilePath  = abspath(join(dirname(__file__), 'traefik.toml'))
-
-    traefik        = Popen(["traefik", "-c", configFilePath],
+    traefik         = Popen(["traefik", "-c", configFilePath],
                             stdout=PIPE)
-    defaultBackend = Popen([sys.executable, dummyServerPath, str(defaultBackendPort)],
-                            stdout=PIPE)
-    firstBackend   = Popen([sys.executable, dummyServerPath, str(firstBackendPort)],
-                            stdout=PIPE)
-    secondBackend  = Popen([sys.executable, dummyServerPath, str(secondBackendPort)],
-                            stdout=PIPE)
+    defaultBackend, firstBackend, secondBackend = traefikUtils.launchBackends()
 
     try:
         """
             Before sending HTTP requests to traefik (and to the backends)
             we need to make sure the services are up and ready
         """
-        assert traefikUtils.checkHostUp("localhost", traefikPort)        == True
-        assert traefikUtils.checkHostUp("localhost", defaultBackendPort) == True
-        assert traefikUtils.checkHostUp("localhost", firstBackendPort)   == True
-        assert traefikUtils.checkHostUp("localhost", secondBackendPort)  == True
+        assert traefikUtils.checkHostUp("localhost", traefikPort) == True
+        traefikUtils.checkBackendsReady()
+        traefikUtils.checkRouting()
 
-        traefikRoutesToCorrectBackend("/otherthings", defaultBackendPort)
-        traefikRoutesToCorrectBackend("/user/somebody", defaultBackendPort)
-        traefikRoutesToCorrectBackend("/user/first", firstBackendPort)
-        traefikRoutesToCorrectBackend("/user/second", secondBackendPort)
-        traefikRoutesToCorrectBackend("/user/first/otherthings", firstBackendPort)
-        traefikRoutesToCorrectBackend("/user/second/otherthings", secondBackendPort)
+    finally:
+        defaultBackend.kill()
+        firstBackend.kill()
+        secondBackend.kill()
+        traefik.kill()
 
+        defaultBackend.wait()
+        firstBackend.wait()
+        secondBackend.wait()
+        traefik.wait()
+
+def test_etcd_routing():
+    traefikPort = traefikUtils.getPort("traefik")
+    traefik        = Popen(["traefik", "--etcd", "--etcd.useapiv3=true"], stdout=None)
+
+    defaultBackend, firstBackend, secondBackend = traefikUtils.launchBackends()
+
+    try:
+        """
+            Before sending HTTP requests to traefik (and to the backends)
+            we need to make sure the services are up and ready
+        """
+        assert traefikUtils.checkHostUp("localhost", traefikPort) == True
+        traefikUtils.checkBackendsReady()
+        traefikUtils.checkTraefikReady()
+        traefikUtils.checkRouting()
     finally:
         defaultBackend.kill()
         firstBackend.kill()
--- traefik/traefikUtils.py
+++ traefik/traefikUtils.py
@@ -1,6 +1,9 @@
 import time
 import socket
-
+import sys
+import requests
+from os.path import abspath, dirname, join
+from subprocess import Popen, PIPE
 
 _ports = {
     "traefik":        8000,
@@ -37,6 +40,61 @@ def checkHostUp(ip, port):
         else:
             time.sleep(delay)
     return up
+
+def traefikRoutesToCorrectBackend(path, expectedPort):
+    baseUrl = "http://localhost:" + str(getPort("traefik"))
+    resp = requests.get(baseUrl + path)
+    """
+        If we get the expected port, it means traefik
+        routed the request to the right backend
+    """
+    assert int(resp.text) == expectedPort
+
+def checkTraefikReady():
+    baseUrl = "http://localhost:" + str(getPort("traefik"))
+
+    timeout = time.time() + 3 # 3 seconds from now
+    ready = False
+    while ready is False:
+        resp = requests.get(baseUrl + "/api/providers/etcdv3")
+        ready = resp.status_code == 200
+        assert time.time() < timeout
+
+    assert ready == True
+
+def checkBackendsReady():
+    defaultBackendPort, firstBackendPort, secondBackendPort = getBackendPorts()
+    assert checkHostUp("localhost", defaultBackendPort) == True
+    assert checkHostUp("localhost", firstBackendPort)   == True
+    assert checkHostUp("localhost", secondBackendPort)  == True
+
+def checkRouting():
+    defaultBackendPort, firstBackendPort, secondBackendPort = getBackendPorts()
+    traefikRoutesToCorrectBackend("/otherthings", defaultBackendPort)
+    traefikRoutesToCorrectBackend("/user/somebody", defaultBackendPort)
+    traefikRoutesToCorrectBackend("/user/first", firstBackendPort)
+    traefikRoutesToCorrectBackend("/user/second", secondBackendPort)
+    traefikRoutesToCorrectBackend("/user/first/otherthings", firstBackendPort)
+    traefikRoutesToCorrectBackend("/user/second/otherthings", secondBackendPort)
+
+def launchBackends():
+    defaultBackendPort, firstBackendPort, secondBackendPort = getBackendPorts()
+    dummyServerPath = abspath(join(dirname(__file__), 'dummyHttpServer.py'))
+
+    defaultBackend = Popen([sys.executable, dummyServerPath, str(defaultBackendPort)],
+                            stdout=PIPE)
+    firstBackend   = Popen([sys.executable, dummyServerPath, str(firstBackendPort)],
+                            stdout=PIPE)
+    secondBackend  = Popen([sys.executable, dummyServerPath, str(secondBackendPort)],
+                            stdout=PIPE)
+    return defaultBackend, firstBackend, secondBackend
+
+def getBackendPorts():
+    defaultBackendPort = getPort("defaultBackend")
+    firstBackendPort   = getPort("firstBackend")
+    secondBackendPort  = getPort("secondBackend")
+
+    return defaultBackendPort, firstBackendPort, secondBackendPort
 
 def generateTraefikToml():
     pass # Not implemented
